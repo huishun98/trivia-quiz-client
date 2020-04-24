770ab420012c10adaeb3dd4704636a6ee2cb507b
Chua Hui Shun <huishun@Chuas-MacBook-Pro.local>
2020-04-24 20:27:05 +0800

    added quiz

---
+++ src/components/index.vue
@@ -0,0 +1,123 @@
+<script>
+import Vue from "vue";
+import axios from "axios";
+import { APIendpoint, quiz, information } from "./../config";
+
+const userResponseSkelaton = Array(quiz.questions.length).fill(null);
+const userResponseTypesSkelaton = Array(quiz.questions.length).fill(null);
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      quiz: quiz,
+      questionIndex: -1,
+      userResponses: userResponseSkelaton,
+      userTypes: userResponseTypesSkelaton,
+      isActive: false
+    };
+  },
+  filters: {
+    charIndex: function(i) {
+      return String.fromCharCode(97 + i);
+    }
+  },
+
+  methods: {
+    // link: function() {
+    //    var currentURL=location.href;
+    //    // var currentURLascii=unescape(location.href);
+    //    console.log(currentURL)
+    // },
+    restart: function() {
+      this.questionIndex = 0;
+      this.userResponses = Array(this.quiz.questions.length).fill(null);
+    },
+    selectOption: function(index) {
+      Vue.set(this.userResponses, this.questionIndex, index);
+      Vue.set(
+        this.userTypes,
+        this.questionIndex,
+        this.quiz.questions[this.questionIndex].responses[index].type
+      );
+      // console.log(this.userResponses);
+    },
+    next: function() {
+      if (this.questionIndex < this.quiz.questions.length) this.questionIndex++;
+    },
+    prev: function() {
+      if (this.quiz.questions.length > 0) this.questionIndex--;
+    },
+    quizCompleted() {
+      if (this.questionIndex >= this.quiz.questions.length) {
+        this.sendResults();
+        return true;
+      }
+      return false;
+    },
+    sendResults: function() {
+      let answers = [];
+      for (let i = 0; i < this.userResponses.length; i++) {
+        answers.push();
+      }
+      let results = {};
+      for (let i = 0; i < quiz.questions.length; i++) {
+        results[`q${i + 1}`] =
+          quiz.questions[i].responses[this.userResponses[i]].text;
+      }
+      // console.log(results);
+      results.result = this.shorthandResult();
+
+      axios({
+        method: "post",
+        url: APIendpoint,
+        data: results
+      })
+        .then(response => console.log(response))
+        .catch(err => console.log(`Error when sending results: ${err}`));
+    },
+    // Return "true" count in userResponses
+    shorthandResult: function() {
+      const collateTypes = Array.prototype.concat.apply([], this.userTypes);
+
+      let result = {};
+      for (let i = 0; i < collateTypes.length; i++) {
+        if (!result[collateTypes[i]]) {
+          result[collateTypes[i]] = 0;
+        }
+        ++result[collateTypes[i]];
+      }
+
+      const order = ["TWGA", "TSGW", "TPCB", "TSSW", "TCLG", "TRC"];
+
+      return Object.keys(result).reduce((a, b) => {
+        if (result[a] > result[b]) {
+          return a;
+        }
+        if (result[a] < result[b]) {
+          return b;
+        }
+        if (result[a] == result[b]) {
+          return order.indexOf[a] < order.indexOf[b] ? a : b;
+        }
+      });
+    },
+    score: function() {
+      return information[this.shorthandResult()];
+      // let score = {
+      //    TWGA: 0,
+      //    TSGW: 0,
+      //    TPCB: 0,
+      //    TSSW: 0,
+      //    TCLG: 0,
+      //    TRC: 0
+      // };
+      //return this.userResponses.filter(function(val) { return val }).length;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="../styles/index.css"></style>
+<template src="./index.html"></template>
---
+++ src/config.js
@@ -0,0 +1,157 @@
+const APIendpoint = "http://127.0.0.1:5000/";
+
+const quiz = {
+    user: "Dave",
+    questions: [
+        {
+            text: "Do you know what day is it today?",
+            responses: [
+                { text: "Of course!", type: ["TSGW"] },
+                { text: "Of course, it's Friday everyday!", type: ["TCLG"] },
+                { text: "No, I've totally lost track of time.", type: ["TSSW"] },
+                { text: "Does it matter?", type: ["TRC"] }
+            ]
+        },
+        {
+            text: "What do you think of the term 'circuit breaker'?",
+            responses: [
+                {
+                    text: "Very creative name! (No sarcasm intended)",
+                    type: ["TSGW", "TSSW"]
+                },
+                { text: "Lockdown then say lockdown lah...", type: ["TWGA", "TPCB"] },
+                { text: "I catch no ball/ I don't understand.", type: ["TCLG"] },
+                { text: "So smart I cannot even.", type: ["TRC"] }
+            ]
+        },
+        {
+            text: "How is working from home/ home-based learning for you?",
+            responses: [
+                {
+                    text: "What is that! I'm an essential worker.",
+                    type: ["TRC", "TSGW"]
+                },
+                {
+                    text:
+                        "I'm an essential worker but I think I ought/ want to be home.",
+                    type: ["TPCB"]
+                },
+                { text: "I'm so enjoying it.", type: ["TCLG"] },
+                { text: "It's even more tiring.", type: ["TSSW"] }
+            ]
+        },
+        {
+            text: "What is your opinion on the timing of the circuit breaker?",
+            responses: [
+                { text: "Too late! Too slow!", type: ["TWGA"] },
+                { text: "Just timely!", type: ["TSGW"] },
+                {
+                    text: "No need CB lah!",
+                    type: ["TPCB"]
+                },
+                { text: "KNNBCCB", type: ["TRC"] }
+            ]
+        },
+        {
+            text: "Which scenario is the worst to you?",
+            responses: [
+                { text: "WIFI down", type: ["WIFI down"] },
+                {
+                    text: "Ran out of toilet paper",
+                    type: ["TPCB"]
+                },
+                { text: "Another extension in the circuit breaker", type: ["TWGA"] },
+                { text: "Food delivery got lost", type: ["TSSW"] }
+            ]
+        },
+        {
+            text:
+                "Which scene (from a movie/ series) does COVID-19 resemble the most to you?",
+            responses: [
+                {
+                    text: "Thanos snapping his fingers in Avengers: Infinity War",
+                    type: ["TPCB"]
+                },
+                { text: "The zombie apocalypse in Train to Busan", type: ["TRC"] },
+                { text: "The political mess in Chernobyl", type: ["TWGA"] },
+                { text: "I don't watch movies.", type: ["TSSW"] }
+            ]
+        },
+        {
+            text:
+                "Which category of app did you download/ use the most during the circuit breaker?",
+            responses: [
+                { text: "Shopping app", type: ["TSSW"] },
+                { text: "Government-related app", type: ["TSGW"] },
+                {
+                    text: "Gaming app",
+                    type: ["TCLG"]
+                },
+                { text: "Social media app", type: ["TWGA"] }
+            ]
+        },
+        {
+            text: "What do you look forward to each day?",
+            responses: [
+                {
+                    text: "The number of confirmed (and unlinked etc.) cases lor!",
+                    type: ["TRC"]
+                },
+                {
+                    text:
+                        "I don't look forward to the next day nor the day after next and the day after.",
+                    type: ["TPCB"]
+                },
+                {
+                    text: "Knowing that tomorrow will be a better day :)",
+                    type: ["TSGW", "TCLG"]
+                },
+                { text: "A better government.", type: ["TWGA"] }
+            ]
+        }
+    ]
+}
+
+const information = {
+    TWGA: {
+        persona: "The Wise Gahmen Advisor",
+        description:
+            "You're not comfortable with how things are being done and you would have done it better.",
+        notes: "Gahmen: Singlish spelling of government."
+    },
+    TSGW: {
+        persona: "The Supportive Gung-ho Warrior",
+        description:
+            "You think the government is already doing their best and try your best to help in any way.",
+        notes:
+            "Gung-ho: Unthinkingly enthusiastic and eager, especially about taking part in fighting or warfare"
+    },
+    TPCB: {
+        persona: "The Pek Chek Buddy",
+        description:
+            "You're frustrated by the current situation and a little upset that you can't help or do much about it.",
+        notes:
+            "Pek Chek: exasperated or frustrated (This feeling of helpless exasperation arises when one faces an urgent problem but is unable to solve it)"
+    },
+    TSSW: {
+        persona: "The Solemn Silent Worker",
+        description:
+            "You're trying to stay positive and it's proving to be challenging.",
+        notes: ""
+    },
+    TCLG: {
+        persona: "The Calm Lepak Guru",
+        description:
+            "You're taking things in stride and are trying to keep your optimism.",
+        notes: "Lepak: Relax or chill out by doing nothing."
+    },
+    TRC: {
+        persona: "The Random Calefare",
+        description:
+            "You feel like you're in a simulation and you just happened to be part of it.",
+        notes:
+            "Calefare: a film extra, or an actor with a minor, non-speaking role."
+    }
+}
+
+export { APIendpoint, quiz, information }
