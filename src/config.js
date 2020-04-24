5c5005d81f99e2319bf1b3d785afd581323647a0
Chua Hui Shun <huishun@Chuas-MacBook-Pro.local>
2020-04-24 20:27:48 +0800

    removed user

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const APIendpoint = "http://127.0.0.1:5000/";
 
 const quiz = {
-    user: "Dave",
+    user: "",
     questions: [
         {
             text: "Do you know what day is it today?",
