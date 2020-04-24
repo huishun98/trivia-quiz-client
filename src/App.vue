adbcbf18d6072d80ab2e7622d6cb22eea8d57bb4
Chua Hui Shun <huishun@Chuas-MacBook-Pro.local>
2020-04-24 21:50:23 +0800

    changed document title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <vue-headful title="Circuit Breaker Personality"/>
     <Quiz />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueHeadful from 'vue-headful';
 
+Vue.component('vue-headful', vueHeadful);
 Vue.config.productionTip = false
 
 new Vue({
