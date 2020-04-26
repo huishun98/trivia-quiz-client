348bd341e855dc58ae9adb14a4cc3e8f0bfd8e13
Chua Hui Shun <huishun@Chuas-MacBook-Pro.local>
2020-04-26 16:02:24 +0800

    edited title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <vue-headful title="Circuit Breaker Personality"/>
     <Quiz />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vueHeadful from 'vue-headful';
 
-Vue.component('vue-headful', vueHeadful);
 Vue.config.productionTip = false
 
 new Vue({
